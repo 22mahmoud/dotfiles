6247955d743444df02f259b58fc2e64579b0abb8
22mahmoud <hello@mahmoudashraf.dev>
2022-02-24 22:59:52 +0200

    qutebrowser updates

--- qutebrowser/.config/qutebrowser/binding.py
+++ qutebrowser/.config/qutebrowser/binding.py
@@ -32,6 +32,9 @@ config.unbind('d')
 config.unbind('D')
 config.bind('x', 'tab-close')
 config.bind('X', 'undo')
+config.bind(',xb', 'config-cycle statusbar.show always never')
+config.bind(',xt', 'config-cycle tabs.show always never')
+config.bind(',xx', 'config-cycle statusbar.show always never;; config-cycle tabs.show always never')
 
 # for old.reddit.com expand thread.
 config.set('hints.selectors', {'preview': ['.expando-button']}, pattern='*://*.reddit.com/*')
