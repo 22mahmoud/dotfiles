475a2105b91e4ad653790ad45a3ad645bfbb24c5
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2024-12-21 15:44:59 +0200

    update

--- qutebrowser/.config/qutebrowser/theme.py
+++
@@ -1,7 +0,0 @@
-from qutebrowser.config.configfiles import ConfigAPI
-from qutebrowser.config.config import ConfigContainer
-
-config: ConfigAPI = config  # type: ignore
-c: ConfigContainer = c  # type: ignore
-
-config.source('base16.py')
