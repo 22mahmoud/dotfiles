d99fc1449bfdd8c7f82605be8eb7c7569ac1c728
Mahmoud Ashraf <hello@mahmoudashraf.dev>
2025-01-15 15:51:09 +0200

    update

--- qutebrowser/.config/qutebrowser/binding.py
+++ qutebrowser/.config/qutebrowser/binding.py
@@ -7,6 +7,8 @@ config: ConfigAPI = config  # type: ignore
 config.bind("V", "spawn mpx -a {url}")
 config.bind(",v", "hint links spawn mpx -a {hint-url}")
 
+config.bind("<ctrl+b>", "spawn --userscript linkwarden")
+
 qute_rbw = "spawn --userscript qute-rbw "
 config.bind(",pl", qute_rbw)
 config.bind(",pu", f"{qute_rbw} --username-only")
